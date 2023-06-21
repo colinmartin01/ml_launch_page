8d238f7ecf518dc9c7a4436b915cf776727938f4
colinmartin01 <colin.martin@appsassociates.com>
2023-06-21 12:59:26 -0400

    Set up 3 main parts
    Logo-header, App-header, App-body; with Apps Associates logo, site headline and Apps website link, and important links, respectively

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import apps_logo from './apps_logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+      <header className="Logo-header">
+        <img src={apps_logo} className="App-logo" alt="logo" />
+      </header>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Machine Learning Safety Stock Model
+        </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.appsassociates.com/leadership-team/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Our Website
         </a>
       </header>
+
+      <body className="App-body">
+        <p>
+          Important Links
+        </p>
+        <a
+          className="App-link"
+          href="https://appsassc.us-east-1.snowflakecomputing.com/console/login#/?returnUrl=internal%2Fworksheet"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Snowflake Console
+        </a>
+        <p>
+          SageMaker
+        </p>
+        <a
+          className="App-link"
+          href="https://aws.amazon.com/pm/sagemaker/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Pre-Processing
+        </a>
+        <a
+          className="App-link"
+          href="https://aws.amazon.com/pm/sagemaker/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Studio
+        </a>
+        <a
+          className="App-link"
+          href="https://aws.amazon.com/pm/sagemaker/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Canvas
+        </a>
+      </body>
+
     </div>
   );
 }

e921a0061c6287db91cc35f7a31f4fb333da34d2
colinmartin01 <colin.martin@appsassociates.com>
2023-07-06 12:04:46 -0400

    Added GraphQL

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "mllaunchpage": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "mllaunchpage0889afea": {
       "AppClientID": "string",

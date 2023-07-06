6d5c077f9d5c9ba789f9abe3bf21d9a798d5dd3b
colinmartin01 <colin.martin@appsassociates.com>
2023-07-06 12:12:49 -0400

    added s3 capability

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3a122f770": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
